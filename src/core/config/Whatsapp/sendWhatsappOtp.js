c9aaa18abb315b60cc34e9e28261c7cb6458a1c4
anish-singh-rawat <www.computernetwork@gmail.com>
2026-08-01 12:52:54 +0530

    update token

--- src/core/config/Whatsapp/sendWhatsappOtp.js
+++ src/core/config/Whatsapp/sendWhatsappOtp.js
@@ -4,7 +4,7 @@ import FormData from 'form-data';
 dotenv.config();
 
 const WHATSAPP_BASE_URL   = process.env.WHATSAPP_BASE_URL   || "https://digiwppconnect-backend.digibysr.in";
-const WHATSAPP_DEVICE_TOKEN = process.env.WHATSAPP_DEVICE_TOKEN || "32200dde-249d-4ef9-ab9d-be7414120e12";
+const WHATSAPP_DEVICE_TOKEN = process.env.WHATSAPP_DEVICE_TOKEN || "29a959f6-e5ee-46e5-80b7-603d8dc92efd";
 
 const DEFAULT_PHONE         = process.env.WHATSAPP_DEFAULT_PHONE || "917579440117";
 
